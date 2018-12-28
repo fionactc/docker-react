d8ae4827e3de63139a72fc664b96af767ddc2fd1
Fiona Cheung <fionach@hk1.ibm.com>
2018-12-28 12:22:46 +0800

    feature

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>I am here</p>
+          <p>I am here with Feature!</p>
           <a
             className="App-link"
             href="https://reactjs.org"
